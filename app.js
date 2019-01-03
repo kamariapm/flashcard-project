334ac58336648407ca86a9d1dca12f9615dc49aa
COR Students <corstudents@Matts-MacBook-Pro-108.local>
2019-01-03 17:13:02 -0500

    added files

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const path = require('path');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const MongoClient = require('mongodb').MongoClient;
+// const path = require('path');
 
 
-const app = express()
-app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
+// const app = express()
+// app.set('view engine', 'pug');
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 
-let db
+// let db
 
 // function parseMongoData(mongoObject) {
 //     const jObject = {
@@ -20,8 +20,56 @@ let db
 //     }
 //     console.log("insideparseMongo",jObject)
 //     return jObject
-    
+
 // }
+
+// MongoClient.connect('mongodb://sandbox:project1@ds117888.mlab.com:17888/flashcards', (err, database) => {
+//     if (err) return console.log(err)
+//     db = database.db('flashcards')
+//     app.listen(3000, function () {
+//         console.log('Listening on port 3000')
+//     })
+// })
+
+
+// app.get('/', (req, res) => {
+//     let cursor = db.collection('flashcards').find().toArray(function(err, results){
+//         if (err) return console.log(err)
+//         // const js_results = results.map(flashcard => parseMongoData(flashcard))
+//         // console.log(js_results)
+//         // const test_object = {val1: 1, val2: 2}
+//         res.render('index.pug', {flashcards: (results)})
+//     }) 
+// }) 
+
+// , test_value:(results[0].question), js_results: JSON.stringify(js_results), test_object: JSON.stringify(test_object)}) 
+
+// app.post('/flashcards', (req, res) => {
+
+//     db.collection('flashcards').save(req.body, (err,result) =>{
+//         console.log(req.body)
+//         if (err) return console.log(err)
+//         console.log('saved to database')
+//         console.log(req.body) 
+//         res.redirect('/')
+//     })
+// })
+
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const path = require('path');
+const app = express()
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+let db
+let triviaCards = []
 
 MongoClient.connect('mongodb://sandbox:project1@ds117888.mlab.com:17888/flashcards', (err, database) => {
     if (err) return console.log(err)
@@ -32,25 +80,45 @@ MongoClient.connect('mongodb://sandbox:project1@ds117888.mlab.com:17888/flashcar
 })
 
 
-app.get('/', (req, res) => {
-    let cursor = db.collection('flashcards').find().toArray(function(err, results){
-        if (err) return console.log(err)
-        // const js_results = results.map(flashcard => parseMongoData(flashcard))
-        // console.log(js_results)
-        // const test_object = {val1: 1, val2: 2}
-        res.render('index.pug', {flashcards: (results)})
-    }) 
-}) 
 
-// , test_value:(results[0].question), js_results: JSON.stringify(js_results), test_object: JSON.stringify(test_object)}) 
+app.get('/', (req, res) => {
+    //.find.toArray makes it a JSON file
+    let cursor = db.collection('flashcards').find().toArray(function (err, results) {
+        if (err) return console.log(err)
+        triviaCards = results
+        console.log(results)
+        res.render('index.pug', { flashcards: results })
+    })
+})
+
+app.get('/flashcards', (req, res) => {
+    res.send(triviaCards)
+    console.log(triviaCards)
+})
 
 app.post('/flashcards', (req, res) => {
-    
-    db.collection('flashcards').save(req.body, (err,result) =>{
+
+    db.collection('flashcards').save(req.body, (err, results) => {
         console.log(req.body)
         if (err) return console.log(err)
         console.log('saved to database')
-        console.log(req.body) 
+        console.log(req.body)
+        res.redirect('/')
+    })
+})
+
+
+app.put('/flashcards/:id', (req, res) => {
+    console.log('in put' + req.params.id)
+    let item = {
+        question: req.body.question,
+        hint: req.body.hint,
+        answer:req.body.answer
+    }
+    let id = req.body.id
+    db.collection('flashcards').updateOne({_id:ObjectId(id)}, {$set:item}, (err, results) => {
+        if (err) return console.log(err)
+        console.log('PUT to database')
         res.redirect('/')
     })
 })
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,24 +1,252 @@
+$('#flashcardsLoaded').hide()
+$('#addCard').hide()
+$("#answer").hide()
+$("#hint").hide()
+$('#flashcardsUpdated').hide()
+$('#update').hide()
+$('#cardID').hide()
 
 
+//setting variables to count through arrays in the object and pass them into the object
+let cardNumber = 0
+let flashcardDB = []
+//create function that counts through the number of the arrays in the object 
+//and returns the number of the array back to 0 when it is to
+//begin at the first card again
+let question
+let hint
+let answer
+function setUpFlashcard() {
+    console.log(flashcardDB[cardNumber]._id)
+    question = flashcardDB[cardNumber].question
+    hint = flashcardDB[cardNumber].hint
+    answer = flashcardDB[cardNumber].answer
+    //printing the info from database in the pug/html
+    $("#question").html(flashcardDB[cardNumber].question)
+    $("#hint").html(flashcardDB[cardNumber].hint)
+    $("#answer").html(flashcardDB[cardNumber].answer)
+    cardNumber++
+    console.log(cardNumber)
+    if (cardNumber >= flashcardDB.length) {
+        cardNumber = 0
+    }
+}
 
-$(document).ready (function(){
-// $(".flashcardsPost").hide()
-// $(".flashcards").hide()
-
-// let flashCards1 = JSON.parse(flashcards)
-console.log("hi")
-// console.log(JSON.parse(flashcards))
-// console.log(flashcards)
-// console.log(test_value)
-// console.log(JSON.parse(js_results))
-console.log(test_object)
-$('#flashcardsUpload').hide()
-
+function checkForInput () {
+    let empty = true
+    while ($('#addCardQuest').val() || $('#addCardAnsw').val() || $('#addCardHint').val() == '') {
+        console.log('waiting for new flashcard')
+        // if ($('#addCardQuest').val() || $('#addCardAnsw').val() || $('#addCardHint').val() == ''){
+            $('#ifEmptyStrings').show()
+            empty
+        // $('#ifEmptyStrings').on('shown.bs.modal', function () {
+     if ($('#addCardQuest').val() && $('#addCardAnsw').val() && $('#addCardHint').val() !== '') {
+        empty == false
+     }
 
 
+    }
+}
+//making the ajax call to recieve information from the database
+
+$.ajax({
+    type: 'GET',
+    url: '/flashcards',
+    success: function (data) {
+        console.log(data)
+        //setting variable for the data to come back as an array
+        flashcardDB = data
+        setUpFlashcard()
+    },
+    error: function (error) {
+        console.log(error)
+
+    }
+})
 
 
+$.ajax({
+    type: 'PUT',
+    url: '/flashcards',
+    success: function (data) {
+        console.log('put' + data)
+        //setting variable for the data to come back as an array
+        flashcardDB = data
+        setUpFlashcard()
+    },
+    error: function (error) {
+        console.log(error)
+    }
+})
 
 
+$(document).ready(function () {
+    $('#begin').click(function () {
+        $('#flashcardsLoaded').show();
+        $('#addCard').show();
+        $('#welcomeCard').hide();
+        $('#begin').hide();
+        $('#welcomeToWebsite').hide();
+        $('#update').show()
+
+    })
+    $('#flashcardsUploaded').hide();
+
+    $('#submitNext').click(function () {
+        $("#answer").hide();
+        $("#hint").hide();
+
+        setUpFlashcard()
+        console.log("flashcardDB", flashcardDB)
+    });
+
+    $("#submitHint").click(function () {
+        $("#hint").show();
+    });
+
+    $("#submitAnswer").click(function () {
+        $("#answer").show();
+
+    });
+//Edit card button
+    $('#update').click(function(){
+        $('#flashcardsUpdated').show();
+        $('#updateCardQuest').val(question)
+        $('#updateCardHint').val(hint)
+        $('#updateCardAnsw').val(answer)
+
+    })
+
+    $('#submitPut').click(function () {
+      
+    })
+    
+    $('#cancelPut').click(function () {
+        $('#flashcardsUpdated').hide();
+        $('#flashcardsLoaded').show();
+
+    })
+
+    $('#addCard').click(function () {
+        $('#flashcardsLoaded').hide();
+        $('#flashcardsUploaded').show();
+        $('#ifEmptyStrings').hide();
+
+    })
+    $('#submit').click(function () {
+        // checkForInput()
+        // ($('#addCardQuest').val(' ') && $('#addCardAnsw').val(' ') && $('#addCardHint').val(' ')) {
+        //     M.toast({ html: `Added to the deck!`, inDuration: 500 })
+    })
+    
+    $('#cancel').click(function () {
+        $('#flashcardsUploaded').hide();
+        $('#flashcardsLoaded').show();
+
+    })
 
 })
+
+
+
+
+
+
+
+// let cardNumber = 0
+// let flashcard = []
+
+
+
+// function setupFlashcard(cardNumber , data){
+//     console.log(data)
+
+//     if(cardNumber < data.length){
+//         //printing the info from database in the pug/html
+//         $("#question").html(data[cardNumber].question)
+//         $("#answer").html(data[cardNumber].answer)
+//         $("#hint").html(data[cardNumber].hint)
+//     } else {
+//         cardNumber=0
+//     }        
+// } 
+
+// function hideShowHint() {
+//     $('#hint').is(":hidden") ? $('#hint').show() : $('#hint').hide()
+// }
+
+// function hideShowAnswer() {
+//     $('#answer').is(":hidden") ? $('#answer').show() : $('#answer').hide()
+// }
+
+
+
+//  $.ajax(
+//     {
+//         type: 'GET',
+//         url: '/flashcards',
+//         success: function (data) {
+//             // $(document).ready(function () {
+//                 //calling the function
+//                 setupFlashcard(cardNumber, data)
+//                 //making the buttons work
+//                 $("#submitNext").click(function () {
+//                     // $("#question").show()
+//                     cardNumber ++
+//                     if(cardNumber > data.length){
+//                         cardNumber = 0
+//                     } 
+
+//                     setupFlashcard(cardNumber, data)   
+//                 });
+
+//                 $("#submitHint").click(function () {
+//                    hideShowHint()
+
+//                 });
+
+//                 $("#submitAnswer").click(function () {
+//                     hideShowAnswer()
+
+//                  });
+
+//              $('#addCard').click(function(){
+//                 // $('.modal').modal();
+//                 $('#flashcardsLoaded').hide()
+//                 $('#flashcardsUploaded').show()
+//              })
+//              $('#submit').click(function(){
+//                 // if ($('#addCardQuest') && $('#addCardAnsw') && $('#addCardHint') === ' ') {
+//                 //     console.log('waiting for new flashcard')
+//                 //     alert('waiting for new flashcard')
+
+//                 // } else if ($('#addCardQuest') && $('#addCardAnsw') && $('#addCardHint') != ' ') {
+//                 //     M.toast({html: `Added to the deck!`, inDuration: 500})
+//                 // }
+//              })
+//              $('#cancel').click(function(){
+//                 $('#flashcardsUploaded').hide()
+//                 $('#flashcardsLoaded').show()
+
+//              })
+
+//             // });
+//          },
+//         error: function (err) {
+//             console.log(err)
+//         }
+
+
+//     });
+
+// function hideShowHint() {
+//     $('#hint').is(":hidden") ? $('#hint').show() : $('#hint').hide()
+// }
+
+// function hideShowAnswer() {
+//     $('#answer').is(":hidden") ? $('#answer').show() : $('#answer').hide()
+// }
+
+// function hideShowHintAnswer () {
+//     if ()
+// }
